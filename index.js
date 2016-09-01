98004666f20bb5c8c8c3b4fb33c5f01bc27b7cf5
Wes Todd <wes@wesleytodd.com>
2016-09-01 16:48:01 -0500

    oops, wrong comparison

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var stack = require('callsite');
 module.exports = function runnable (fnc, defaults, ctx) {
 	var s = stack();
 	// If called directly just run it with the defaults
-	if (module.parent.filename === s[s.length - 1].getFileName()) {
+	if (require.main.filename === s[1].getFileName()) {
 		return fnc.apply(ctx || null, defaults || []);
 	}
 
