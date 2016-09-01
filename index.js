86bf77ac4a3dba33c12f37b1f18b27a32fe5121c
Wes Todd <wes@wesleytodd.com>
2016-09-01 16:00:13 -0500

    swap detection method for callsite module

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var stack = require('callsite');
+
 module.exports = function runnable (fnc, defaults, ctx) {
+	var s = stack();
 	// If called directly just run it with the defaults
-	if (module.parent && module.parent.parent === null) {
+	if (module.parent.filename === s[s.length - 1].getFileName()) {
 		return fnc.apply(ctx || null, defaults || []);
 	}
 
